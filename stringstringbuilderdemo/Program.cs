6916ec8dffed3d07417e80a33e52fdd42f31171e
Patan Moulali <patan.moulali@mphasis.com>
2022-08-24 11:13:53 +0530

    Add project files.

---
+++ stringstringbuilderdemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringstringbuilderdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt = DateTime.Now;
+            DateTime dob = new DateTime();
+
+            Console.Write("Enter your date of birth : ");
+            dob = Convert.ToDateTime(Console.ReadLine());
+            TimeSpan age = dt.Subtract(dob);
+
+            Console.WriteLine("Your age in Years : {0}", dt.Year - dob.Year);
+            Console.WriteLine("Your age in months : {0}, ", dt.Month - dob.Month);
+            Console.WriteLine("Is birthyear leapyear: {0}", DateTime.IsLeapYear(dob.Year));
+            Console.WriteLine("day of the week : {0}", dob.DayOfWeek);
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
